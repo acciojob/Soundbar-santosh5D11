78973a4c80517baf75800b55777a25eef3aa8e11
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-17 23:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+let audio;
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+
+        // Stop previous sound
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+
+        // Get button text
+        const soundName = button.innerText;
+
+        // Create audio
+        audio = new Audio(`./sounds/${soundName}.mp3`);
+
+        // Play audio
+        audio.play();
+    });
+});
+
+stopButton.addEventListener("click", function () {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+});
